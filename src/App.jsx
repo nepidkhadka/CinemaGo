6a7cebcda471997db58bf9a7a03367d75f98ac28
Dipen <dipen@example.com>
2023-08-19 22:51:50 +0545

    HomePage Fetch & Display

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './/Components/Header/header';
+import Home from './Page/Home/Home'
 
 function App() {
 
   return (
     <>
-      
+    <Router>
+    <Header/>
+        <Routes>
+          <Route index element = {<Home/>}></Route>
+          <Route path="movie/:id" element={ <h1>Movie Detail Page</h1> } ></Route>
+          <Route path='movie/:type' element={ <h1>Movie List Page</h1> } ></Route>
+          <Route path='/*' element={ <h1>404 ! Not Found</h1> } ></Route>
+        </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+
+
+function header() {
+    return (
+        <div className='header' >
+            <div className='headerLeft' >
+                <Link to="/"> <img className='header_icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png'/></Link>
+                <Link to="/movie/popular" className='header_icon'>Popular</Link>
+                <Link to="/movie/top_rated" className='header_icon'>Top Rated</Link>
+                <Link to="/movie/upcoming" className='header_icon'>Upcoming</Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default header
